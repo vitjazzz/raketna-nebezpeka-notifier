9ec17c5d238d97103b49583646c227a22c2ad2e5
vzikratyi <vzikratyi@playtika.com>
2025-02-07 00:23:24 +0200

    Add control to check if script works

--- get_messages.py
+++ get_messages.py
@@ -2,6 +2,7 @@ from telethon import TelegramClient, events
 from playsound import playsound
 import requests
 import time
+import asyncio
 
 from dotenv import load_dotenv
 import os
@@ -12,6 +13,7 @@ API_ID = os.getenv("API_ID")
 API_HASH = os.getenv("API_HASH")
 PHONE_NUMBER = os.getenv("PHONE_NUMBER")
 CHANNEL = os.getenv("CHANNEL")
+CONTROL_CHANNEL = os.getenv("CONTROL_CHANNEL")
 
 PUSHCUT_API_KEY = os.getenv("PUSHCUT_API_KEY")
 NOTIFICATION_NAME = "My%20First%20Notification"
@@ -40,16 +42,27 @@ async def new_message_handler(event):
             print("Keyword detected! Playing sound.")
             for url in urls:
                 requests.post(url, headers=headers)
-            playsound('alarm.wav')
             playsound('alarm.mp3')
             time.sleep(5)
             for url in urls:
                 requests.post(url, headers=headers)
         else:
             print("No matching keywords in the message.")
-    else:
-        print("Received a message with no text.")
-    
+
+
+message_text = f"Аналізую канал '{CHANNEL}', підписників - {len(urls)}."
+
+async def send_message_periodically():
+    while True:
+        try:
+            await client.send_message(CONTROL_CHANNEL, message_text)
+        except Exception as e:
+            print("Error sending message:", e)
+        await asyncio.sleep(2 * 3600)
+
+
+with client:
+    client.loop.run_until_complete(send_message_periodically())
 
 print("Listening for new messages...")
 client.run_until_disconnected()
